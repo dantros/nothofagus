abf1f18f6c010da619b1e225c2fa383e6663da9d
Daniel Calderon <21376367+dantros@users.noreply.github.com>
2024-08-23 22:35:52 -0400

    Hello Nothofagus working :)

--- examples/hello_nothofagus.cpp
+++ examples/hello_nothofagus.cpp
@@ -23,12 +23,12 @@ int main()
         {1.0, 1.0, 1.0, 0.0}
     };
 
-    /*Nothofagus::ColorPallete pallete2{
+    Nothofagus::ColorPallete pallete2{
         {0.0, 0.0, 0.0, 0.0},
         {0.0, 0.4, 0.0, 1.0},
         {0.2, 0.8, 0.2, 1.0},
         {0.5, 1.0, 0.5, 1.0},
-    };*/
+    };
 
     Nothofagus::Texture texture1({ 4, 4 }, { 0.5, 0.5, 0.5, 1.0 });
     texture1.setPallete(pallete1)
@@ -42,7 +42,7 @@ int main()
     );    
     Nothofagus::TextureId textureId1 = canvas.addTexture(texture1);
     
-    /*Nothofagus::Texture texture2({8, 8}, {0.5, 0.5, 0.5, 1.0});
+    Nothofagus::Texture texture2({8, 8}, {0.5, 0.5, 0.5, 1.0});
     texture2.setPallete(pallete2)
         .setPixels(
         {
@@ -56,13 +56,13 @@ int main()
             2,1,3,0,0,3,2,1,
         }
     );
-    Nothofagus::TextureId textureId2 = canvas.addTexture(texture2);*/
+    Nothofagus::TextureId textureId2 = canvas.addTexture(texture2);
 
     Nothofagus::BellotaId bellotaId1 = canvas.addBellota({{{0.0f, 0.0f}, 15.0, 0.125}, textureId1});
-    /*Nothofagus::BellotaId bellotaId2 = canvas.addBellota({{{0.0f, 0.75f}}, textureId1});
-    Nothofagus::BellotaId bellotaId3 = canvas.addBellota({ {{-0.5f, -0.5f}}, textureId2 });
-    Nothofagus::BellotaId bellotaId4 = canvas.addBellota({ {{0.0f, 0.0f}}, textureId2 });
-    */
+    Nothofagus::BellotaId bellotaId2 = canvas.addBellota({{{0.0f, 0.75f}, -15.0, 0.125}, textureId1});
+    Nothofagus::BellotaId bellotaId3 = canvas.addBellota({ {{-0.5f, -0.5f}, 0.0, 0.125}, textureId2 });
+    Nothofagus::BellotaId bellotaId4 = canvas.addBellota({ {{0.5f, 0.52f}, 15.0, 0.025}, textureId2 });
+    
     canvas.run();
 
     /*bool movingRight = true;
