63fe35167055717ef03925d6b9459603e8575e20
Sebastian Mira Pacheco <sebastianandresmira@gmail.com>
2024-12-05 20:08:13 -0300

    animation state and animation state machine

---
+++ source/animation_state.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <graaflib/graph.h>
+#include <vector>
+namespace Nothofagus
+{
+
+class AnimationState
+{
+};
+
+}
+
---
+++ source/animation_state_machine.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <graaflib/graph.h>
+#include <vector>
+namespace Nothofagus
+{
+
+class AnimationStateMachine
+{
+};
+
+}
+
+
