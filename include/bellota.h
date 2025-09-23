8304636b87ebe547929cd9786c5c59f89e2b3153
Daniel Calderon <21376367+dantros@users.noreply.github.com>
2025-09-22 23:54:01 -0700

    Adding current layer member variable to Bellota definition. Default is 0 so it does not interfer with standard non-animated bellota

--- include/bellota.h
+++ include/bellota.h
@@ -34,6 +34,7 @@ public:
     Bellota(Transform transform, TextureId textureId):
         mTransform( transform ),
         mTextureId{ textureId },
+        mCurrentLayer{ 0 },
         mDepthOffset{ 0 },
         mVisible{ true }
         
@@ -43,6 +44,7 @@ public:
     Bellota(Transform transform, TextureId textureId, std::int8_t depthOffset) :
         mTransform( transform ),
         mTextureId{ textureId },
+        mCurrentLayer{ 0 },
         mDepthOffset{ depthOffset },
         mVisible{ true }
 
@@ -61,9 +63,13 @@ public:
     const bool& visible() const { return mVisible; }
     bool& visible() { return mVisible; }
 
+    const std::size_t& currentLayer() const { return mCurrentLayer; }
+    std::size_t& currentLayer() { return mCurrentLayer; }
+
 private:
     Transform mTransform;
     TextureId mTextureId;
+    std::size_t mCurrentLayer;        /**< The current layer being displayed */
 
     /* Greater values means closer to the viewer. Default is 0. Example, a background image could use depth offset -1. */
     std::int8_t mDepthOffset;
