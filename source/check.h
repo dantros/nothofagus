72f74b15dc651111160ae6a79f3aa61fc3028489
Daniel Calderon <21376367+dantros@users.noreply.github.com>
2025-10-05 02:36:04 -0700

    order fix (?)

--- source/check.h
+++ source/check.h
@@ -7,28 +7,6 @@
 namespace Nothofagus
 {
     /**
-     * @brief Checks if a given condition is valid during debug builds, throwing an exception otherwise.
-     * 
-     * This function is only active in debug builds. If the given condition is not met, it logs an error 
-     * and throws an exception. It helps ensure that the code behaves as expected during the development phase.
-     * 
-     * @param condition The condition to check.
-     * @param errorMessage The error message to log if the condition is not valid.
-     */
-    /* Check for the given condition to be valid on debug builds, throws otherwise. */
-    inline void debugCheck(bool condition, const std::string& errorMessage)
-    {
-        #ifdef _DEBUG
-        runtimeCheck(condition, errorMessage);
-        #endif
-    }
-
-    inline void debugCheck(bool condition)
-    {
-        debugCheck(condition, "");
-    }
-
-    /**
      * @brief Checks if a given condition is valid during runtime, throwing an exception otherwise.
      * 
      * This function is always active, regardless of the build type. It checks the given condition and if it's 
@@ -50,5 +28,27 @@ namespace Nothofagus
     inline void runtimeCheck(bool condition)
     {
         runtimeCheck(condition, "");
+    }
+
+        /**
+     * @brief Checks if a given condition is valid during debug builds, throwing an exception otherwise.
+     * 
+     * This function is only active in debug builds. If the given condition is not met, it logs an error 
+     * and throws an exception. It helps ensure that the code behaves as expected during the development phase.
+     * 
+     * @param condition The condition to check.
+     * @param errorMessage The error message to log if the condition is not valid.
+     */
+    /* Check for the given condition to be valid on debug builds, throws otherwise. */
+    inline void debugCheck(bool condition, const std::string& errorMessage)
+    {
+        #ifdef _DEBUG
+        runtimeCheck(condition, errorMessage);
+        #endif
+    }
+
+    inline void debugCheck(bool condition)
+    {
+        debugCheck(condition, "");
     }
 }
