82ee2b4c859acfb29708c5ff4966b7dd3849ebd3
Daniel Calderon <21376367+dantros@users.noreply.github.com>
2026-06-09 22:48:29 -0700

    throwing runtime exception to fail gracefully

--- source/check.h
+++ source/check.h
@@ -1,16 +1,17 @@
 #pragma once
 
 #include <string>
+#include <stdexcept>
 #include <spdlog/spdlog.h>
 
 namespace Nothofagus
 {
     /**
      * @brief Checks if a given condition is valid during runtime, throwing an exception otherwise.
-     * 
-     * This function is always active, regardless of the build type. It checks the given condition and if it's 
+     *
+     * This function is always active, regardless of the build type. It checks the given condition and if it's
      * not met, it logs an error and throws an exception. It is useful for runtime validation in all builds.
-     * 
+     *
      * @param condition The condition to check.
      * @param errorMessage The error message to log if the condition is not valid.
      */
@@ -20,7 +21,7 @@ namespace Nothofagus
         if (not condition)
         {
             spdlog::error("Check failed: {}", errorMessage);
-            throw;
+            throw std::runtime_error("Nothofagus check failed: " + errorMessage);
         }
     }
 
